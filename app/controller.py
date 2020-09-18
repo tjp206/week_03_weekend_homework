b62a1514036e26af2ad09272cbd6e31687eacba0
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-09-18 17:10:56 +0100

    fixed bug in controller

--- app/controller.py
+++ app/controller.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from app import app
-from app.models.player import import *
+from app.models.player import *
 from app.models.game import *
 
 @app.route('/')
