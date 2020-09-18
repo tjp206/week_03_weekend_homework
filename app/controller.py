ddde0ad68637ea29dcb174b0748b11b13aa2cd54
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-09-18 17:05:43 +0100

    updated html files & added background colour

--- app/controller.py
+++ app/controller.py
@@ -3,3 +3,6 @@ from app import app
 from app.models.player import import *
 from app.models.game import *
 
+@app.route('/')
+def index():
+    return render_template('index.html', title='Home', ) #rock_paper_scissors = rock_paper_scissors?
