aef9b475e6c20f967d55753d1a444b0c9e293a10
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-09-19 16:45:56 +0100

    centered image/button & text

--- app/models/player.py
+++ app/models/player.py
@@ -1,5 +1,7 @@
 class Player:
 
-    def __init__(self, name, choice):
+    def __init__(self, name, rock, paper, scissors):
         self.name = name
-        self.choice = choice
+        self.rock = rock
+        self.paper = paper
+        self.scissors = scissors
