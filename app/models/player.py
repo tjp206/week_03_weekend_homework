9b9c75b409f04555fbffeaed8d4a06bbc7c59ddb
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-09-20 19:54:51 +0100

    updated changes in player class

--- app/models/player.py
+++ app/models/player.py
@@ -2,10 +2,10 @@ from app.models.game import *
 
 class Player:
 
-    def __init__(self, rock, paper, scissors):
-        self.rock = rock
-        self.paper = paper
-        self.scissors = scissors
+    def __init__(self, name, choice):
+        self.name = name
+        self.choice = choice
+        
 
 
     
